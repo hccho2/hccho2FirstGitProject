7f91e12f1337aa182935385465ef5e709dbdf275
hccho2 <hccho2@gmail.com>
2017-04-11 23:50:34 +0900

    update at home

--- ConvolutionNet_test.py
+++ ConvolutionNet_test.py
@@ -48,6 +48,45 @@ class Convolution:
         dx = col2im_back(dcol, self.x.shape, FH, FW, self.stride, self.pad)
 
         return dx
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+        
+        self.x = None
+        self.arg_max = None
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h*self.pool_w)
+
+        arg_max = np.argmax(col, axis=1)
+        out = np.max(col, axis=1)
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.arg_max = arg_max
+
+        return out
+
+    def backward(self, dout):
+        dout = dout.transpose(0, 2, 3, 1)
+        
+        pool_size = self.pool_h * self.pool_w
+        dmax = np.zeros((dout.size, pool_size))
+        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
+        dmax = dmax.reshape(dout.shape + (pool_size,)) 
+        
+        dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
+        dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
+        
+        return dx
 
 
 def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
@@ -166,11 +205,12 @@ x1 = np.array([[[[9, 9, 0, 7, 8, 4, 6],
          [1, 5, 6, 5, 5, 7, 6]]]])
 W = np.random.randn(2,3,5,5)
 b = np.zeros(2)
-col1 =im2col(x1,5,5,stride=2,pad=0)    
-x2 = col2im(col1,(1,3,7,7),5,5,stride=2,pad=0)
+
 
 convnet = Convolution(W,b,stride=2, pad=0)
 
-xx = convnet.forward(x1)
-yy = convnet.backward(xx)
-print(xx.shape,yy.shape)
+x2 = convnet.forward(x1)
+poolnet = Pooling(2,2,stride=2,pad=0)
+x3 = poolnet.forward(x2)
+
+y = poolnet.backward(x3)
