d96fdff0f33029b665cce59b0148c0d7ef9a9ff6
hccho2 <hccho2@gmail.com>
2017-04-11 18:05:02 +0900

    ConvolutionNet

---
+++ ConvolutionNet_test.py
@@ -0,0 +1,176 @@
+# coding: utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+        
+        # 중간 데이터（backward 시 사용）
+        self.x = None   
+        self.col = None
+        self.col_W = None
+        
+        # 가중치와 편향 매개변수의 기울기
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = 1 + int((H + 2*self.pad - FH) / self.stride)
+        out_w = 1 + int((W + 2*self.pad - FW) / self.stride)
+
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        col_W = self.W.reshape(FN, -1).T
+
+        out = np.dot(col, col_W) + self.b
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)  # hccho: col2im(out,(N,FN,out_h,out_w),1,1,stride=1,pad=0)과 동일
+
+        self.x = x
+        self.col = col
+        self.col_W = col_W
+
+        return out
+
+    def backward(self, dout):
+        FN, C, FH, FW = self.W.shape
+        dout = dout.transpose(0,2,3,1).reshape(-1, FN)  #forward의 transpose(0,3,1,2)의 역변환
+
+        self.db = np.sum(dout, axis=0)
+        self.dW = np.dot(self.col.T, dout)
+        self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
+
+        dcol = np.dot(dout, self.col_W.T)
+        dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
+
+        return dx
+
+
+def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
+    """다수의 이미지를 입력받아 2차원 배열로 변환한다(평탄화).
+    
+    Parameters
+    ----------
+    input_data : 4차원 배열 형태의 입력 데이터(이미지 수, 채널 수, 높이, 너비)
+    filter_h : 필터의 높이
+    filter_w : 필터의 너비
+    stride : 스트라이드
+    pad : 패딩
+    
+    Returns
+    -------
+    col : 2차원 배열
+    """
+    N, C, H, W = input_data.shape
+    out_h = (H + 2*pad - filter_h)//stride + 1
+    out_w = (W + 2*pad - filter_w)//stride + 1
+
+    img = np.pad(input_data, [(0,0), (0,0), (pad, pad), (pad, pad)], 'constant')
+    col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
+
+    for y in range(filter_h):
+        y_max = y + stride*out_h
+        for x in range(filter_w):
+            x_max = x + stride*out_w
+            col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
+
+    col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N*out_h*out_w, -1)
+    return col
+
+def col2im_back(col, input_shape, filter_h, filter_w, stride=1, pad=0):
+    """(im2col과 반대) 2차원 배열을 입력받아 다수의 이미지 묶음으로 변환한다.
+    
+    Parameters
+    ----------
+    col : 2차원 배열(입력 데이터)
+    input_shape : 원래 이미지 데이터의 형상（예：(10, 1, 28, 28)）
+    filter_h : 필터의 높이
+    filter_w : 필터의 너비
+    stride : 스트라이드
+    pad : 패딩
+    
+    Returns
+    -------
+    img : 변환된 이미지들
+    """
+    N, C, H, W = input_shape
+    out_h = (H + 2*pad - filter_h)//stride + 1
+    out_w = (W + 2*pad - filter_w)//stride + 1
+    col = col.reshape(N, out_h, out_w, C, filter_h, filter_w).transpose(0, 3, 4, 5, 1, 2)
+
+    img = np.zeros((N, C, H + 2*pad + stride - 1, W + 2*pad + stride - 1))
+    for y in range(filter_h):
+        y_max = y + stride*out_h
+        for x in range(filter_w):
+            x_max = x + stride*out_w
+            img[:, :, y:y_max:stride, x:x_max:stride] += col[:, :, y, x, :, :]
+
+    return img[:, :, pad:H + pad, pad:W + pad]    
+def col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0):
+    """(im2col과 반대) 2차원 배열을 입력받아 다수의 이미지 묶음으로 변환한다.
+    
+    Parameters
+    ----------
+    col : 2차원 배열(입력 데이터)
+    input_shape : 원래 이미지 데이터의 형상（예：(10, 1, 28, 28)）
+    filter_h : 필터의 높이
+    filter_w : 필터의 너비
+    stride : 스트라이드
+    pad : 패딩
+    
+    Returns
+    -------
+    img : 변환된 이미지들
+    """
+    N, C, H, W = input_shape
+    out_h = (H + 2*pad - filter_h)//stride + 1
+    out_w = (W + 2*pad - filter_w)//stride + 1
+    col = col.reshape(N, out_h, out_w, C, filter_h, filter_w).transpose(0, 3, 4, 5, 1, 2)
+
+    img = np.zeros((N, C, H + 2*pad + stride - 1, W + 2*pad + stride - 1))
+    for y in range(filter_h):
+        y_max = y + stride*out_h
+        for x in range(filter_w):
+            x_max = x + stride*out_w
+            img[:, :, y:y_max:stride, x:x_max:stride] = col[:, :, y, x, :, :]
+
+    return img[:, :, pad:H + pad, pad:W + pad]     
+#x1 = np.random.rand(1,3,7,7)
+#x1 = np.random.randint(10,size=(1,3,7,7))
+x1 = np.array([[[[9, 9, 0, 7, 8, 4, 6],
+         [8, 1, 5, 0, 2, 3, 5],
+         [8, 9, 1, 7, 3, 9, 2],
+         [2, 2, 9, 1, 3, 0, 5],
+         [1, 4, 2, 7, 5, 0, 4],
+         [0, 2, 6, 9, 3, 2, 6],
+         [8, 1, 1, 6, 9, 1, 5]],
+
+        [[7, 7, 2, 1, 5, 5, 4],
+         [5, 9, 4, 6, 4, 4, 4],
+         [2, 9, 4, 2, 4, 2, 5],
+         [2, 0, 5, 6, 0, 8, 5],
+         [3, 9, 4, 9, 7, 5, 4],
+         [8, 7, 2, 0, 2, 5, 3],
+         [8, 3, 5, 7, 5, 4, 7]],
+
+        [[7, 5, 4, 4, 6, 9, 0],
+         [4, 9, 0, 3, 9, 1, 0],
+         [6, 5, 5, 3, 6, 3, 7],
+         [7, 1, 9, 8, 2, 7, 4],
+         [5, 1, 3, 8, 2, 7, 7],
+         [4, 6, 2, 8, 3, 3, 8],
+         [1, 5, 6, 5, 5, 7, 6]]]])
+W = np.random.randn(2,3,5,5)
+b = np.zeros(2)
+col1 =im2col(x1,5,5,stride=2,pad=0)    
+x2 = col2im(col1,(1,3,7,7),5,5,stride=2,pad=0)
+
+convnet = Convolution(W,b,stride=2, pad=0)
+
+xx = convnet.forward(x1)
+yy = convnet.backward(xx)
+print(xx.shape,yy.shape)
