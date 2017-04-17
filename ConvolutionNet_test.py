423969b6a1d4c01a6e146b7600bc0498be33d347
hccho2 <hccho2@gmail.com>
2017-04-17 18:01:34 +0900

    comment update

--- ConvolutionNet_test.py
+++ ConvolutionNet_test.py
@@ -195,7 +195,6 @@ class SimpleConvNet:
 
 class DeepConvNet:
     """정확도 99% 이상의 고정밀 합성곱 신경망
-
     네트워크 구성은 아래와 같음
         conv - relu - conv- relu - pool -
         conv - relu - conv- relu - pool -
@@ -216,7 +215,11 @@ class DeepConvNet:
         wight_init_scales = np.sqrt(2.0 / pre_node_nums)  # ReLU를 사용할 때의 권장 초깃값
         
         self.params = {}
+        # 계층 생성===========
+        self.layers = []
+
         pre_channel_num = input_dim[0]
+
         for idx, conv_param in enumerate([conv_param_1, conv_param_2, conv_param_3, conv_param_4, conv_param_5, conv_param_6]):
             self.params['W' + str(idx+1)] = wight_init_scales[idx] * np.random.randn(conv_param['filter_num'], pre_channel_num, conv_param['filter_size'], conv_param['filter_size'])
             self.params['b' + str(idx+1)] = np.zeros(conv_param['filter_num'])
@@ -226,27 +229,21 @@ class DeepConvNet:
         self.params['W8'] = wight_init_scales[7] * np.random.randn(hidden_size, output_size)
         self.params['b8'] = np.zeros(output_size)
 
-        # 계층 생성===========
-        self.layers = []
-        self.layers.append(Convolution(self.params['W1'], self.params['b1'], 
-                           conv_param_1['stride'], conv_param_1['pad']))
+
+
+        self.layers.append(Convolution(self.params['W1'], self.params['b1'], conv_param_1['stride'], conv_param_1['pad']))
         self.layers.append(Relu())
-        self.layers.append(Convolution(self.params['W2'], self.params['b2'], 
-                           conv_param_2['stride'], conv_param_2['pad']))
+        self.layers.append(Convolution(self.params['W2'], self.params['b2'], conv_param_2['stride'], conv_param_2['pad']))
         self.layers.append(Relu())
         self.layers.append(Pooling(pool_h=2, pool_w=2, stride=2))
-        self.layers.append(Convolution(self.params['W3'], self.params['b3'], 
-                           conv_param_3['stride'], conv_param_3['pad']))
+        self.layers.append(Convolution(self.params['W3'], self.params['b3'], conv_param_3['stride'], conv_param_3['pad']))
         self.layers.append(Relu())
-        self.layers.append(Convolution(self.params['W4'], self.params['b4'],
-                           conv_param_4['stride'], conv_param_4['pad']))
+        self.layers.append(Convolution(self.params['W4'], self.params['b4'], conv_param_4['stride'], conv_param_4['pad']))
         self.layers.append(Relu())
         self.layers.append(Pooling(pool_h=2, pool_w=2, stride=2))
-        self.layers.append(Convolution(self.params['W5'], self.params['b5'],
-                           conv_param_5['stride'], conv_param_5['pad']))
+        self.layers.append(Convolution(self.params['W5'], self.params['b5'], conv_param_5['stride'], conv_param_5['pad']))
         self.layers.append(Relu())
-        self.layers.append(Convolution(self.params['W6'], self.params['b6'],
-                           conv_param_6['stride'], conv_param_6['pad']))
+        self.layers.append(Convolution(self.params['W6'], self.params['b6'], conv_param_6['stride'], conv_param_6['pad']))
         self.layers.append(Relu())
         self.layers.append(Pooling(pool_h=2, pool_w=2, stride=2))
         self.layers.append(Affine(self.params['W7'], self.params['b7']))
@@ -1060,7 +1057,7 @@ def Test2():
     
     
 def Test3():
-    # Test2의 결과를 받아서...
+    # Test2(SimpleConvNet)의 결과를 받아서...
     network = SimpleConvNet(input_dim=(1,28,28), 
                             conv_param = {'filter_num': 30, 'filter_size': 5, 'pad': 0, 'stride': 1},
                             hidden_size=100, output_size=10, weight_init_std=0.01)    
