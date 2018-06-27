0b02e34b741d70a617caa9b728b66e2396eacb00
Heecheol Cho <hccho2@gmail.com>
2018-06-27 13:24:23 +0900

    Update tensorflowTest.py

--- tensorflowTest.py
+++ tensorflowTest.py
@@ -425,7 +425,7 @@ LSTMStateTuple(c=array([[-0.08561244, -0.71315455],[-0.02546103, -0.3122089 ]], 
     import tensorflow as tf
     tf.reset_default_graph()
     from tensorflow.contrib.framework.python.framework import checkpoint_utils
-    checkpoint_dir = 'D:\\hccho\\cs231n-Assignment\\assignment3\\save-sigle-layer\\model.ckpt-1000000' # 구체적으로 명시
+    checkpoint_dir = 'D:\\hccho\\cs231n-Assignment\\assignment3\\save-sigle-layer\\model.ckpt-1000000.ckpt' # 구체적으로 명시
     #checkpoint_dir = 'D:\\hccho\\cs231n-Assignment\\assignment3\\save-sigle-layer # 디렉토리만 지정 ==> 가장 최근
     var_list = checkpoint_utils.list_variables(checkpoint_dir)
     #sess = tf.Session()
