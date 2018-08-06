6be0f31a94da6f13f8effcfe4683a4cb8edd070a
Heecheol Cho <hccho2@gmail.com>
2018-08-06 10:13:06 +0900

    Add files via upload

--- gTTS_test.py
+++ gTTS_test.py
@@ -3,12 +3,16 @@ import json
 import numpy as np
 from gtts import gTTS
 
-from pygame import mixer
+
 import os
 import pyglet   # AVbin이라는 프로그램을 성치해야 함(http://avbin.github.io/AVbin/Download.html)
 
+def exit_callback(dt):
+    pyglet.app.exit()
+
+
 filename = "hello.mp3"
-tts = gTTS(text='안녕하세요. 조희철이라고 합니다. 박일재 친구에요', lang='ko', slow=False)
+tts = gTTS(text='안녕하세요. 조희철이라고 합니다. 현종석친구에요', lang='ko', slow=False)
 
 tts.save(filename)
 
@@ -18,5 +22,10 @@ tts.save(filename)
 
 song = pyglet.media.load(filename)
 song.play()
+
+pyglet.clock.schedule_once(exit_callback , song.duration)  # 이게 있어야 ,pyglet.app.run()이 종료됨
+
+
 pyglet.app.run()
 
+print("Done")
