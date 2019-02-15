f8db885430394be952e2bc35af1890fe4a7457f4
Heecheol Cho <hccho2@gmail.com>
2019-02-15 16:47:57 +0900

    Update RNNCell-Wrapper-hccho.py

--- RNNCell-Wrapper-hccho.py
+++ RNNCell-Wrapper-hccho.py
@@ -104,7 +104,7 @@ class MyRnnHelper(Helper):
 
     @property
     def sample_ids_shape(self):
-        return tf.TensorShape([])
+        return tf.TensorShape([])   # sample_ids의 shape이 (batch_size,) 이므로, batch_size를 제외하면, "[]"이 된다.
 
     def next_inputs(self, time, outputs, state,sample_ids, name=None):   # time+1을 위한 input을 만든다., outputs,state,sample_ids는 time step에서의 결과이다.
         # 넘어오는 sample_ids는 sample 함수에어 계산된어 넘어온 값이다.   <----- 이런 계산은 BasicDecoder의 'step' 함수에서 이루어 진다.
