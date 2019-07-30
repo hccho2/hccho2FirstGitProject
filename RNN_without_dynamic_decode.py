f2f45c96b11dfb58e3c2926d05cf8b0fccd47be6
Heecheol Cho <hccho2@gmail.com>
2019-07-30 14:49:50 +0900

    Add files via upload

--- RNN_without_dynamic_decode.py
+++ RNN_without_dynamic_decode.py
@@ -31,29 +31,99 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+# TensorFlow에서는 5가지의 로깅 타입을 제공하고 있습니다. ( DEBUG, INFO, WARN, ERROR, FATAL ) INFO가 설정되면, 그 이하는 다 출력된다.
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+
 def test1():
+    # free forcing
     batch_size = 2
     hidden_dim = 3
     input_dims = 2
     T = 5
     init_inputs = tf.placeholder(tf.float32, shape=(batch_size,input_dims))
-    h0 = tf.zeros(shape=(batch_size,hidden_dim),dtype=tf.float32)
+    init_state = tf.zeros(shape=(batch_size,hidden_dim),dtype=tf.float32)
     
     cell = tf.contrib.rnn.BasicRNNCell(num_units=hidden_dim)
     output_layer = tf.layers.Dense(input_dims, name='output_projection')
     BN = tf.layers.BatchNormalization()
     outs = []
-    inputs = init_inputs
-    for _ in range(T):
-        a = cell(inputs,h0)
-        h0 = a[0]
-        out = output_layer(a[1])
-        out = BN(out)
-        inputs = out
-        outs.append(out)
     
+    
+    inputs = init_inputs
+    new_state = init_state
+    for _ in range(T):
+        new_state,outputs = cell(inputs,new_state)
+
+        outputs = output_layer(outputs)
+        outputs = BN(outputs)
+        inputs = outputs
+        outs.append(outputs)
+    
+    outs = tf.stack(outs,axis=1)
     print(outs)
     print(tf.trainable_variables())
+    
+
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    
+    inp = np.random.randn(batch_size,input_dims)
+    X = sess.run(outs,feed_dict={init_inputs: inp})
+    print(X.shape,'\n', X)
+    
+
+    
+    
+    
+def test11():
+    # teacher forcing vs dynamic rnn
+    batch_size = 2
+    hidden_dim = 3
+    input_dims = 2
+    T = 5
+    inputs = tf.placeholder(tf.float32, shape=(batch_size,T,input_dims))
+    init_state = tf.zeros(shape=(batch_size,hidden_dim),dtype=tf.float32)
+    
+    cell = tf.contrib.rnn.BasicRNNCell(num_units=hidden_dim)
+    output_layer = tf.layers.Dense(input_dims, name='output_projection')
+    BN = tf.layers.BatchNormalization()
+    
+    
+    outs = []
+    new_state = init_state
+    for i in range(T):
+        new_state,outputs = cell(inputs[:,i,:],new_state)
+
+        outputs = output_layer(outputs)
+        outputs = BN(outputs)
+        outs.append(outputs)
+    
+    outs = tf.stack(outs,axis=1)
+    print(outs)
+    print(tf.trainable_variables())
+    
+
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    
+    inp = np.random.randn(batch_size,T,input_dims)
+    X = sess.run(outs,feed_dict={inputs: inp})
+    print("for loop: ", X.shape,'\n', X)
+
+
+    outs2, last_state = tf.nn.dynamic_rnn(cell,inputs,sequence_length=[T]*batch_size,initial_state=init_state) 
+    outs2 = output_layer(outs2)
+    outs2 = BN(tf.reshape(outs2,[-1,input_dims])) # BN 적용을 위해 2-dim으로 
+    outs2 = tf.reshape(outs2,(batch_size,-1,input_dims)) # 다시 3-dim으로
+
+    Y = sess.run(outs2,feed_dict={inputs: inp})
+    print("dynamic_rnn: ", Y.shape,'\n', Y)
+
+    print(np.allclose(X,Y))
+
+
+
 
 def test2():
     # tf.keras.layers.SimpleRNN 이용하기
@@ -113,6 +183,6 @@ def test3():
     print(tf.trainable_variables())
 
 
-test3()
-    
+#test1()
+test11()    
 print("Done")
