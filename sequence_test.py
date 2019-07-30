26ca59a1212391934fce8de4e2ba3624b60715f4
Heecheol Cho <hccho2@gmail.com>
2019-07-30 15:04:11 +0900

    Add files via upload

--- sequence_test.py
+++ sequence_test.py
@@ -43,3 +43,15 @@ tokenizer.fit_on_texts(sentences_merged)
 print(tokenizer.word_index)  # {'안녕': 1, '어제는': 2, '뭐': 3, '했어': 4, '반가워': 5, '또': 6, '봐요': 7}
 
 sentences_id = tokenizer.texts_to_sequences(sentences_merged) # [[1, 2, 3, 4, 5], [1, 6, 2, 7, 8]]
+
+###############################################
+# 단어 빈도
+###############################################
+
+import itertools
+import nltk
+x = ["This is a foobar-like sentence.","Good muffins cost $3.88\nin New York.  Please buy me\ntwo of them.\nThanks."]
+y= [nltk.word_tokenize(sent) for sent in x]
+w = list(itertools.chain(*y))  # list 하나로 합치기
+z = nltk.FreqDist(w)    # zz=collections.Counter(w) 같은 결과
+vocab = z.most_common(5) # zz.most_common(5)
