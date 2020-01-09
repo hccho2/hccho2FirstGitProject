bfe22fc55fbdbdfafc4309f5564166d3fe5c4548
Heecheol Cho <hccho2@gmail.com>
2020-01-09 10:49:48 +0900

    Add files via upload

--- pca.py
+++ pca.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 from scipy.fftpack import fft
 import librosa
 from tensorflow.python.layers.core import Dense
+#from sklearn.decomposition import PCA
 theta = np.arctan(1.5)   # np.pi/4
 N=5000
 
@@ -27,13 +28,21 @@ C = (1/N)*np.matmul(X.T,X)
 p,v = np.linalg.eig(C)  # column vector가 eigenvector
 
 q=np.argsort(-p)
-v = v[:,q]
+w = v[:,q]
 
 
-Y=np.matmul(v.T,X.T).T
+Y=np.matmul(w.T,X.T).T
 
 plt.scatter(Y[:,0],Y[:,1],s=1)
 plt.axis([-10, 10, -10, 10])
 plt.show()
 
 
+##### sklearn의 PCA
+pca = PCA(n_components=2)  # 뽑아낼 component 갯수 지정
+#pca = PCA(0.8)  # 80%까지의 component를 추출
+principalComponents = pca.fit_transform(X)
+
+# 먼저 fit을 하게 되면, normalization을 수행한다.
+pca.fit(X)
+principalComponents2 = pca.fit_transform(X)
