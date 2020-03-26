131657922ee4bba9c7e557c0c112bcc63b91e71d
Heecheol Cho <hccho2@gmail.com>
2020-03-26 21:08:01 +0900

    Update tensorflowTest.py

--- tensorflowTest.py
+++ tensorflowTest.py
@@ -177,28 +177,32 @@ def testPlaceholde():
 def testLinearRegression():
     import tensorflow as tf    
     
-    x_train = [1,2,3]
-    y_train = [1,2,3]
-    
-    W = tf.Variable(tf.random_normal([1]),name='Weight')
-    b = tf.Variable(tf.random_normal([1]),name='bias')
-    
-    hypothesis = x_train * W + b
-    
-    cost = tf.reduce_mean(tf.square(hypothesis - y_train))
-    
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.01)
-    train = optimizer.minimize(cost)
-    
-    
-    sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
-    
-    for i in range(4000):
-        sess.run(train)
-        
-        if i%100 ==0:
-            print(i,sess.run(cost), sess.run(W), sess.run(b))
+	x_train = [1,2,3]
+	y_train = [1,2,3]
+
+	W = tf.Variable(tf.random_normal([1]),name='Weight')
+	b = tf.Variable(tf.random_normal([1]),name='bias')
+
+	hypothesis = x_train * W + b
+
+	cost = tf.reduce_mean(tf.square(hypothesis - y_train))
+
+	optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.01)
+	train = optimizer.minimize(cost)
+
+
+	#
+	grad1 = tf.gradients(cost,[W,b])  # gradiend 값만
+	grad2 = optimizer.compute_gradients(cost,[W,b])  # gradient와 trainable variable이 쌍으로.
+
+	sess = tf.Session()
+	sess.run(tf.global_variables_initializer())
+
+	for i in range(4000):
+		sess.run(train)
+
+		if i%100 ==0:
+			print(i,sess.run(cost), sess.run(W), sess.run(b))
 
 def testLinearRegression2():
     import tensorflow as tf    
