39e45f1ab8ed5734bce06080c47207f389397b24
Heecheol Cho <hccho2@gmail.com>
2020-05-19 14:20:40 +0900

    Update keras_test.py

--- keras_files/keras_test.py
+++ keras_files/keras_test.py
@@ -302,6 +302,32 @@ def K_function_train():
     print('data', data)
     print('target', target)
     print('predict', model.predict(data))
+
+    
+def keras_layer_test():
+
+    VOCAB_SIZE =20
+    BATCH_SIZE = 3
+    T = 10
+    EMB_SIZE = 8
+    
+    x = np.random.randint(VOCAB_SIZE,size=(BATCH_SIZE,T))
+    xx = tf.convert_to_tensor(x)
+    embedding_layer = keras.layers.Embedding(VOCAB_SIZE,EMB_SIZE)(xx)
+    
+    conv1 = keras.layers.Conv1D(filters=128,kernel_size=3,padding='valid',activation=tf.nn.relu)(embedding_layer)
+    
+    pool1 = keras.layers.GlobalMaxPool1D()(conv1)# (3,128)
+    
+    pool2 = tf.keras.layers.MaxPool1D(conv1.shape.as_list()[1],1)(conv1) #(3,1,128)  
+    
+    
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    a1,a2 = sess.run([pool1,pool2])
+    print(np.allclose(a1,np.squeeze(a2)))
+    
+    
     
 if __name__ == '__main__':
 
