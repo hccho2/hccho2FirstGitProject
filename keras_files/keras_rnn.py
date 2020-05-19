80c6a0ee18c92bd6e2f70e5485b08821bd08dbe3
Heecheol Cho <hccho2@gmail.com>
2020-05-19 14:22:43 +0900

    Update keras_rnn.py

--- keras_files/keras_rnn.py
+++ keras_files/keras_rnn.py
@@ -4,6 +4,29 @@ from keras.models import Model,Sequential
 import tensorflow as tf
 tf.reset_default_graph()
 
+def basic()
+    from tensorflow.keras import layers
+    encoder_vocab = 1000
+    decoder_vocab = 2000
+
+    encoder_input = layers.Input(shape=(None, ))
+    encoder_embedded = layers.Embedding(input_dim=encoder_vocab, output_dim=64)(encoder_input)
+
+    # Return states in addition to output
+    output, state_h, state_c = layers.LSTM( 64, return_state=True, name='encoder')(encoder_embedded)
+    encoder_state = [state_h, state_c]
+
+    decoder_input = layers.Input(shape=(None, ))
+    decoder_embedded = layers.Embedding(input_dim=decoder_vocab, output_dim=64)(decoder_input)
+
+    # Pass the 2 states to a new LSTM layer, as initial state
+    decoder_output = layers.LSTM(64, name='decoder')(decoder_embedded, initial_state=encoder_state)
+    output = layers.Dense(10)(decoder_output)
+
+    model = tf.keras.Model([encoder_input, decoder_input], output)
+    model.summary()
+
+
 def test1():
     vocab_size = 6
     SOS_token = 0
