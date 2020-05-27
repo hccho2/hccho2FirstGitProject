c08e439e0e7ebeea39cbf6f83d06fce1612f451a
Heecheol Cho <hccho2@gmail.com>
2020-05-27 23:51:30 +0900

    Add files via upload

--- TF2/any2.py
+++ TF2/any2.py
@@ -14,7 +14,7 @@ from tensorflow_addons.seq2seq import Sampler
 
 vocab = 20
 embedding_dim = 6
-timestep = 20
+timestep = 20  #encoder time step
 memory_size = 15
 
 attention_cls = tfa.seq2seq.BahdanauAttention
@@ -24,20 +24,19 @@ batch = 3
 
 
 inputs = tf.keras.Input(shape=[timestep])
-encoder_input = tf.keras.layers.Embedding(vocab, embedding_dim, mask_zero=True)(
-    inputs
-)
+encoder_input = tf.keras.layers.Embedding(vocab, embedding_dim, mask_zero=True)(inputs)
+
+
 encoder_output = tf.keras.layers.LSTM(memory_size, return_sequences=True)( encoder_input )
 
 attention = attention_cls(units, encoder_output)
-query = tf.keras.Input(shape=[units])
-state = tf.keras.Input(shape=[timestep])
+query = tf.keras.Input(shape=[units])  # 이전 step attention
+state = tf.keras.Input(shape=[timestep])   # 이전 alignment
 
 score = attention([query, state])
 
 x_test = np.random.randint(vocab, size=(batch, timestep))
 model = tf.keras.Model([inputs, query, state], score)
-
 
 
 
