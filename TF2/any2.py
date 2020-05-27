c8c87e73926333493b200277edf9ba19d0571b18
Heecheol Cho <hccho2@gmail.com>
2020-05-27 17:54:30 +0900

    Add files via upload

---
+++ TF2/any2.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+import numpy as np
+import tensorflow as tf
+import tensorflow_addons as tfa
+
+from tensorflow_addons.seq2seq import Sampler
+
+
+
+
+
+
+
+
+vocab = 20
+embedding_dim = 6
+timestep = 20
+memory_size = 15
+
+attention_cls = tfa.seq2seq.BahdanauAttention
+units = 11
+batch = 3
+
+
+
+inputs = tf.keras.Input(shape=[timestep])
+encoder_input = tf.keras.layers.Embedding(vocab, embedding_dim, mask_zero=True)(
+    inputs
+)
+encoder_output = tf.keras.layers.LSTM(memory_size, return_sequences=True)( encoder_input )
+
+attention = attention_cls(units, encoder_output)
+query = tf.keras.Input(shape=[units])
+state = tf.keras.Input(shape=[timestep])
+
+score = attention([query, state])
+
+x_test = np.random.randint(vocab, size=(batch, timestep))
+model = tf.keras.Model([inputs, query, state], score)
+
+
+
+
+
+
+
+
+
+
+
