583afbed51ddee8359852ccb1047748ff160d561
Heecheol Cho <hccho2@gmail.com>
2020-11-21 18:28:36 +0900

    Add files via upload

--- torch_files/torchvision_test.py
+++ torch_files/torchvision_test.py
@@ -12,11 +12,14 @@ C:\Anaconda3\Lib\site-packages\torchvision\models   ---> 이곳에서 모델 구
 '''
 import torch
 import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import torchvision
 import torchvision.models as models
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 import torch.nn.functional as F
 from torch import nn
+import matplotlib.pyplot as plt
 import os
 from PIL import Image
 def test1():
@@ -61,6 +64,7 @@ class MyVGG(nn.Module):
         return x  
 def test2():
     # 책 "파이토치 첫걸음" pp 75
+    # vgg --> MyVGG로 modify
     os.environ['TORCH_HOME'] = './pretrained'
     vgg16 = models.vgg16(pretrained=True, progress=True)  # 540M
  
@@ -87,7 +91,9 @@ def test2():
     
     print('Done')
 def test3():
+    # resnet50, inceptin_v3의 pretrained weight로 분류해보기.
     # https://github.com/pytorch/vision/issues/484   ---> imagenet_classes.txt, imagenet_synsets.txt
+    # https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
 
     with open('imagenet_synsets.txt', 'r') as f:
         synsets = f.readlines()
@@ -140,7 +146,7 @@ def test3():
     
     print('Done')
 def test4():
-    # 책 "파이토치 첫걸음" pp 74
+    # ImageFolder, SubsetRandomSampler 사용하기.
     # Dataset: 상속하여 class로 작성. __len__(), __getitem__() 필수 
     
     from torch.utils.data.sampler import SubsetRandomSampler
@@ -173,11 +179,77 @@ def test4():
         print(d[0].shape, d[1])
         if i>10: break    
 
+def torchvision_datast_test():
+    # mnist dataset 사용하기  ---> 다운 받은 data는 PIL.Image.Image(np.array로 변환해 보면 shape(28,28) uint8)이다. transform을 통해 tensor로 변환해야 하다.
+    # (28,28)이기 때문에, reshape이 필요없다.
+    download_root = r'D:\hccho\CommonDataset\mnist'  #---> 아래에 MNIST- raw, proecessed 2개의 subdirectory가 생성된다. 
+    
+    
+    transform = transforms.Compose([transforms.ToTensor()])  # channel dim이 생성(1,28,28)
+
+    
+    
+    train_dataset = datasets.MNIST(download_root, transform=None, train=True, download=True)   # transform을 넣어야 한다.
+    test_dataset = datasets.MNIST(download_root, transform=transform, train=False, download=True)
+    
+    #train_dataset[0][0].show()  # PIL.Image.Image
+    
+    
+    print(len(train_dataset),len(test_dataset))   # 60000, 10000
+
+    test_loader = DataLoader(dataset=test_dataset,batch_size=128,shuffle=True)
+
+    
+    for batch_idx, (x, target) in enumerate(test_loader):
+        if batch_idx % 50 == 0:
+            print(x.shape, target.shape)
+
+
+def transform_test():
+    os.environ['KMP_DUPLICATE_LIB_OK']='True'  # https://jaeniworld.tistory.com/8
+    # https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
+    data_dir = r'D:\hccho\CommonDataset\hymenoptera_data\small'
+
+        
+    
+    
+    data_transforms = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])  # randomness는 data를 epoch마다 달라진다.
+
+    train_dataset =  datasets.ImageFolder(data_dir, data_transforms)
+    dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=5,shuffle=False)
+    class_names = train_dataset.classes
+    print(class_names)
+    inputs, classes = next(iter(dataloader))
+
+    def imshow(inp, title=None):
+        """Imshow for Tensor."""
+        inp = inp.numpy().transpose((1, 2, 0))
+        mean = np.array([0.485, 0.456, 0.406])
+        std = np.array([0.229, 0.224, 0.225])
+        inp = std * inp + mean
+        inp = np.clip(inp, 0, 1)
+        plt.imshow(inp)
+        if title is not None:
+            plt.title(title)
+        plt.pause(0.01)  # pause a bit so that plots are updated
+        plt.show()
+
+
+    out = torchvision.utils.make_grid(inputs)  # inputs: N,3,224,224 ---> (3, 228, 1132)  batch data를 가로로 붙혀, 하나의 이미지로 만든다.
+    imshow(out, title=[class_names[x] for x in classes])
+
+
 if __name__ == '__main__':
     #test1()
-    test2()
+    #test2()
     #test3()
     #test4()
-    
+    #torchvision_datast_test()
+    transform_test()
     
     print('Done')
