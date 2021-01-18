85292c5c79750d3dd101576d9ff9ea4d15239b5e
Heecheol Cho <hccho2@gmail.com>
2021-01-18 20:50:07 +0900

    Add files via upload

--- torch_files/transpose_conv.py
+++ torch_files/transpose_conv.py
@@ -17,7 +17,7 @@ def transepose_conv1():
     H=4;W=3
     kernel_size=2
     pad = 1
-    stride=1
+    stride=2
     image = np.arange(H*W).reshape(1,1,H,W).astype(np.float32)  # (N,C,H,W)
     
     image = torch.tensor(image)
@@ -43,40 +43,43 @@ def transepose_conv1():
 
 def transepose_conv2():
     H=4;W=3
-    kernel_size=2
-    pad = 0
+    pad = 1
     stride=2
-    image = np.arange(H*W).reshape(1,1,H,W).astype(np.float32)  # (N,C,H,W)
+    kernel_size = 3 
+    
+    image = np.arange(H*W).reshape(1,1,H,W).astype(np.float32)  # (N,C,H,W)   random image
     
     image = torch.tensor(image)
     
-    print(image)
+    print("random image", image)
 
-
-    new_image = np.zeros((1,1,(H-1)*stride+1,(W-1)*stride+1))
-
-    new_image[:,:,::stride,::stride] = image
-    new_image = torch.tensor(new_image,dtype=torch.float32)
-
-    print('new image', new_image)
-
-
-
-    weight = torch.tensor([[[[0.1767, 0.1274], [1.0128, 0.4777]]]])
+    weight = torch.randn(1,1,kernel_size,kernel_size)
     
-    out = F.conv_transpose2d(new_image,weight)
     
-    print(f'output size: {(H-1)*stride - 2*pad + kernel_size}')
+    out = F.conv_transpose2d(image,weight,padding=pad,stride=stride)
+
+    print(f'output size: {(H-1)*stride - 2*pad+ + kernel_size} x {(W-1)*stride - 2*pad+ + kernel_size}')
     print(f'weight: {weight}')
     print(f'output: {out}')
+          
+          
+    #####################################################
+    #####################################################
+    # transpose convolution by normal convolution
+    image_stride = np.zeros((1,1,(H-1)*stride+1,(W-1)*stride+1))
+
+    image_stride[:,:,::stride,::stride] = image
+    image_stride = torch.tensor(image_stride,dtype=torch.float32)
+
+    print('new image', image_stride)
     
-    image_padded = F.pad(new_image,(1,1,1,1))
+    image_padded = F.pad(image_stride,(kernel_size-1 - pad,kernel_size-1 - pad,kernel_size-1 - pad,kernel_size-1 - pad))
     
-    print(image_padded)
+    print("padded image", image_padded)
     
     
-    out2 = F.conv2d(image_padded,torch.flip(weight,[2,3]))
-    print(f'output2: {out2}')
+    out2 = F.conv2d(image_padded,torch.flip(weight,[2,3]),padding=0,stride = 1)
+    print(f'manual output: {out2}')
 
 
 
@@ -87,7 +90,6 @@ def transepose_conv2():
 if __name__ == '__main__':
     #transepose_conv1()
     transepose_conv2()
-
 
 
 
