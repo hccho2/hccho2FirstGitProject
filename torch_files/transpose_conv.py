b16f6228cc0baf291705944d49a031b78c81a439
Heecheol Cho <hccho2@gmail.com>
2021-01-21 07:17:48 +0900

    Add files via upload

--- torch_files/transpose_conv.py
+++ torch_files/transpose_conv.py
@@ -1,6 +1,13 @@
 '''
 http://makeyourownneuralnetwork.blogspot.com/2020/02/calculating-output-size-of-convolutions.html
 
+
+"a guide to convolution arithmetic for deep learning"
+
+
+https://github.com/vdumoulin/conv_arithmetic    ---> animation
+
+
 '''
 
 
@@ -17,23 +24,23 @@ def transepose_conv1():
     H=4;W=3
     kernel_size=2
     pad = 1
-    stride=2
+    stride=1
     image = np.arange(H*W).reshape(1,1,H,W).astype(np.float32)  # (N,C,H,W)
     
     image = torch.tensor(image)
     
     print(image)
-    
+
     #weight = torch.randn(1,1,kernel_size,kernel_size)
     weight = torch.tensor([[[[0.1767, 0.1274], [1.0128, 0.4777]]]])
     
-    out = F.conv_transpose2d(image,weight)
+    out = F.conv_transpose2d(image,weight,stride=stride,padding=pad)
     
-    print(f'output size: {(H-1)*stride - 2*pad + kernel_size}')
+    print(f'output size: {(H-1)*stride - 2*pad+ + kernel_size} x {(W-1)*stride - 2*pad + kernel_size}')
     print(f'weight: {weight}')
     print(f'output: {out}')
     
-    image_padded = F.pad(image,(pad,pad,pad,pad))
+    image_padded = F.pad(image,(kernel_size-1-pad,kernel_size-1-pad,kernel_size-1-pad,kernel_size-1-pad))
     
     print(image_padded)
     
@@ -42,7 +49,7 @@ def transepose_conv1():
     print(f'output2: {out2}')
 
 def transepose_conv2():
-    H=4;W=3
+    H=2;W=2
     pad = 1
     stride=2
     kernel_size = 3 
@@ -54,11 +61,11 @@ def transepose_conv2():
     print("random image", image)
 
     weight = torch.randn(1,1,kernel_size,kernel_size)
-    
+
     
     out = F.conv_transpose2d(image,weight,padding=pad,stride=stride)
 
-    print(f'output size: {(H-1)*stride - 2*pad+ + kernel_size} x {(W-1)*stride - 2*pad+ + kernel_size}')
+    print(f'output size: {(H-1)*stride - 2*pad+ + kernel_size} x {(W-1)*stride - 2*pad + kernel_size}')
     print(f'weight: {weight}')
     print(f'output: {out}')
           
@@ -83,13 +90,36 @@ def transepose_conv2():
 
 
 
+def backward_test():
+    
+    image = torch.randn(1,1,4,4,requires_grad=True)
+    weight = torch.randn(1,1,3,3,requires_grad=True)
+    
+    z = F.conv2d(image,weight)
+    print(f'image shape: {image.shape}, weight shape: {weight.shape}, output shape: {z.shape}')
+    print(f'image: {image}, weight: {weight}')
+    print(f'z: {z}')
+
+    z.retain_grad()
+    z.backward(z,retain_graph=True)  # retain_graph=True가 있어야, 아래의 image2.backward에서 error가 나지 않는다.
+    
+    print(f'image.grad: {image.grad}')
 
 
 
+    image2 = F.conv_transpose2d(z,weight)
+    print(f'transposed conv: {image2}')  # image.grad와 일치
+    
+    z.grad.zero_()
+    image2.backward(image)
+    print(f'z.grad: {z.grad}')
+    
+    
 
 if __name__ == '__main__':
     #transepose_conv1()
     transepose_conv2()
+    #backward_test()
 
 
 
